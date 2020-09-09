339bcee854519959a635fcd763100c9f9e45f834
Terrence Regis <terrence.regis@gmail.com>
2020-09-09 09:48:31 -0400

    Fixed flow and disply issues.

--- script.js
+++ script.js
@@ -4,22 +4,30 @@ var divTag1 = document.createElement("A");
 var divTag2 = document.createElement("div");
 var divTag3 = document.createElement("div");
 var divTag4 = document.createElement("div");
-var h5El = document.createElement("h5");
+var divTag5 = document.createElement("div");
+var divTag6 = document.createElement("div");
+var h4El = document.createElement("h4");
 var pEl = document.createElement("p");
+var pEl2 = document.createElement("p");
+var inputEl = document.createElement("input");
 var startButton = document.createElement("button");
 var q1Button = document.createElement("button");
 var q2Button = document.createElement("button");
 var q3Button = document.createElement("button");
 var q4Button = document.createElement("button");
+var submitButton = document.createElement("button");
 
 //Adding content to the created element.
 divTag1.textContent = "View Highscores";
 divTag2.textContent = "Time: 0 ";
-h5El.textContent = "Coding Quiz Challenge";
+h4El.textContent = "Coding Quiz Challenge";
 pEl.textContent =
   "Try to answer the following code-related questions with the time limit. Keep in mind the incorrect answers will penalize your score time by ten seconds!";
 startButton.innerHTML = "Start Quiz";
-//q1Button.innerHTML = "";
+
+//Adding href to anchor tag
+divTag1.href = "./highScores.html";
+console.log((divTag1.href = "./highScores.html"));
 
 //Pending created element to an existing tag
 document.body.appendChild(header);
@@ -27,17 +35,24 @@ header.appendChild(divTag1);
 header.appendChild(divTag2);
 document.body.appendChild(divTag3);
 document.body.appendChild(divTag4);
-divTag3.appendChild(h5El);
+document.body.appendChild(divTag5);
+document.body.appendChild(divTag6);
+divTag3.appendChild(h4El);
 divTag3.appendChild(pEl);
 divTag3.appendChild(startButton);
 divTag4.appendChild(q1Button);
 divTag4.appendChild(q2Button);
 divTag4.appendChild(q3Button);
 divTag4.appendChild(q4Button);
+divTag5.appendChild(pEl2);
+divTag6.appendChild(inputEl);
+divTag6.appendChild(submitButton);
 
 //Styling Element
-h5El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+h4El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
 pEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+pEl2.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+
 divTag1.setAttribute("style", "color:purple;");
 divTag2.setAttribute("style", "float:right;");
 startButton.setAttribute(
@@ -60,6 +75,12 @@ q4Button.setAttribute(
   "style",
   "margin:auto; display:grid; width:auto; float = left; color:white; background:purple;  "
 );
+
+//Hide choices button at start
+q1Button.style.display = "none";
+q2Button.style.display = "none";
+q3Button.style.display = "none";
+q4Button.style.display = "none";
 
 //Defining the quiz as an array of objects
 var quiz = [
@@ -120,14 +141,16 @@ startButton.addEventListener("click", function (event) {
 
 //Timer for questions
 var count = 75;
-
+var score;
+var countDownTimer;
 function startTimer() {
   startButton.style.display = "none";
-  var countDownTimer = setInterval(function () {
+  countDownTimer = setInterval(function () {
     divTag2.textContent = "Time: " + count;
     count--;
     if (count == 0) {
       clearInterval(countDownTimer);
+      gameOver();
     }
   }, 1000);
 }
@@ -135,15 +158,23 @@ function startTimer() {
 var i = 0;
 //Setting up the quiz
 function renderQuestion() {
+  q1Button.style.display = "block";
+  q2Button.style.display = "block";
+  q3Button.style.display = "block";
+  q4Button.style.display = "block";
   if (i <= 4) {
-    h5El.textContent = quiz[i].question;
+    h4El.textContent = quiz[i].question;
     pEl.textContent = "";
-    q1Button.innerHTML = quiz[i].one;
-    q2Button.innerHTML = quiz[i].two;
-    q3Button.innerHTML = quiz[i].three;
-    q4Button.innerHTML = quiz[i].four;
+    pEl2.textContent = "";
+
+    q1Button.textContent = quiz[i].one;
+    q2Button.textContent = quiz[i].two;
+    q3Button.textContent = quiz[i].three;
+    q4Button.textContent = quiz[i].four;
   } else {
+    score = count;
     gameOver();
+    clearInterval(countDownTimer);
   }
 }
 
@@ -156,9 +187,11 @@ divTag4.addEventListener("click", function (event) {
 
   if (result == quiz[i].answer) {
     console.log("Correct!");
+    pEl2.textContent = "Correct!";
   } else {
     count -= 10;
     console.log("Wrong!");
+    pEl2.textContent = "Wrong!";
   }
   i++;
   setTimeout(function () {
@@ -167,5 +200,14 @@ divTag4.addEventListener("click", function (event) {
 });
 
 function gameOver() {
-  console.log("Game Over!");
+  startButton.style.display = "none";
+  q1Button.style.display = "none";
+  q2Button.style.display = "none";
+  q3Button.style.display = "none";
+  q4Button.style.display = "none";
+  h4El.textContent = "All Done!";
+  divTag2.textContent = "Time: " + score; //Not sure why this is not working? It's still showing time from count.
+  console.log((divTag2.textContent = "Time: " + score));
+  pEl.textContent = "You score is " + score;
+  //formEl.submit();
 }
